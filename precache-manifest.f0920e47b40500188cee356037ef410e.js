15b8f81f4c1d87a5c48172939c20ea992646c7bd
karitakrmet <karita.karmet@gmail.com>
2020-01-25 21:01:11 +0200

    Updates

---
+++ precache-manifest.f0920e47b40500188cee356037ef410e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d299d251eab2cc0bd175f285485439b",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "36cbfe3c78858db44848",
+    "url": "/todolist-react/static/css/main.a847029a.chunk.css"
+  },
+  {
+    "revision": "7f0906bc222030ed0f26",
+    "url": "/todolist-react/static/js/2.533f9c5d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist-react/static/js/2.533f9c5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36cbfe3c78858db44848",
+    "url": "/todolist-react/static/js/main.f7721cde.chunk.js"
+  },
+  {
+    "revision": "4e987a9907e11f8f1c44",
+    "url": "/todolist-react/static/js/runtime-main.90c26b64.js"
+  }
+]);
